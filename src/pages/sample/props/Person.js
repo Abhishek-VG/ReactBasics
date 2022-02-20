a8cef3b131f9298a01f7b8edf6959dec9e59015a
Abhishek <abhishekvg@b2btesters.com>
2022-02-20 19:10:58 +0530

    updated

--- src/pages/sample/props/Person.js
+++ src/pages/sample/props/Person.js
@@ -1,14 +1,22 @@
 import React from "react";
 
 const Person = (props) => {
-  const { name, email, gender } = props;
+  const { name, email, gender, status } = props;
   // logics
+  // return (
+  //   <div>
+  //     <div>
+  //       I am a Person, from {name} | email = {email} | gender ={gender}
+  //     </div>
+  //     <br/>
+  //   </div>
+  // );
   return (
-    <div>
-      <div>
-        I am a Person, from {name} | email = {email} | gender ={gender}
-      </div>
-      <br/> 
+    <div className="d-flex p-3 bg-secondary text-white">
+      <div className="p-2 bg-info">I am {name}</div>
+      <div className="p-2 bg-warning">{email}</div>
+      <div className="p-2 bg-primary">{gender}</div>
+      <div className="p-2 bg-warning">{status.toString()}</div>
     </div>
   );
 };
