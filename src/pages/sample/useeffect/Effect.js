c3e9ea1a9176094149504e45913bd7faaf09966b
Abhishek <abhishekvg@b2btesters.com>
2022-02-20 17:51:15 +0530

    added app js

--- src/pages/sample/useeffect/Effect.js
+++ src/pages/sample/useeffect/Effect.js
@@ -1,18 +1,27 @@
 import React from "react";
 
 const Effect = ({ flag }) => {
+  const [count, setCount] = React.useState(0);
   React.useEffect(() => {
     console.log("CHILD MOUNTED");
+    // unmounted
     return () => {
-      console.log("UNMOUNT CALLED")
+      console.log("UNMOUNTED")
     }
   }, []);
   
   React.useEffect(() => {
-    console.log("CHILD MOUNTED/UPDATED");
+    console.log("CHILD MOUNTED + UPDATED");
   });
 
-  return <div>I am effect child isclosed = {flag.toString()}</div>;
+  const handleIncr = () => {
+    setCount(count + 1);
+  }
+  return <>
+  <button onClick={handleIncr}>increment</button>
+  <div>I am children count = {count}</div>
+  <div>I am effect child flag = {flag.toString()}</div>
+  </>;
 };
 
 export default Effect;
--- src/pages/sample/useeffect/EffectParent.js
+++ src/pages/sample/useeffect/EffectParent.js
@@ -3,7 +3,7 @@ import Effect from "./Effect";
 
 const EffectParent = () => {
   const [flag, setFlag] = React.useState(true);
-  const [unmount, setMount] = React.useState(false);
+  const [mount, setMount] = React.useState(true);
 
   // this calls on mount
   React.useEffect(() => {
@@ -19,17 +19,17 @@ const EffectParent = () => {
     setFlag(!flag);
   };
 
-  const handleUnmount = () => {
-    setMount(!flag);
+  const handleMount = () => {
+    setMount(!mount);
   };
 
   return (
     <>
       <h1>Hi, I am Effect parent</h1>
       <button onClick={handleFlag}>Click here to toggle lock</button>
-      <button onClick={handleUnmount}>Click to UNMOUNT</button>
+      <button onClick={handleMount}>Click to UNMOUNT</button>
       <div>-----------------------</div>
-      {!unmount && <Effect flag={flag} />}
+      {mount && <Effect flag={flag} />}
     </>
   );
 };
