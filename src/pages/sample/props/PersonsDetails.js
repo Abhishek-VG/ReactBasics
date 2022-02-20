90427516ee562dc3aeded2e41ece784c484fac6a
Abhishek <abhishekvg@b2btesters.com>
2022-02-20 19:10:51 +0530

    udpated

--- src/pages/sample/props/PersonsDetails.js
+++ src/pages/sample/props/PersonsDetails.js
@@ -1,24 +1,33 @@
 import React from "react";
 import Person from "./Person";
 
-const PersonDetails = (props) => {
+const PersonDetails = () => {
   const [people, setPeople] = React.useState([]);
 
   React.useEffect(() => {
     fetch("https://gorest.co.in/public/v2/users")
-    .then((res) => {
-      console.log(res)
-      return res.json()
-    })
-    .then((res1) => {
-      console.log('res1', res1)
-      setPeople(res1);
-    })
-  }, [])
+      .then((res) => {
+        console.log(res);
+        return res.json();
+      })
+      .then((res1) => {
+        console.log("res1", res1);
+        setPeople(res1);
+      });
+  }, []);
   // logics
   return (
-    <div>
-      {people.map((data) => <Person name={data.name} email={data.email} gender={data.gender} />)}
+    <div className="container mt-3">
+      {[people].map((data) => {
+        return (
+          <Person
+            name={data.name}
+            email={data.email}
+            gender={data.gender}
+            status={data.status}
+          />
+        );
+      })}
     </div>
   );
 };
